25443b75592ef350c4d6cde1dce476755a4515bf
paulo <paulosousa1011@gmail.com>
2020-01-16 22:08:24 -0300

    Adicionando dependencia CORS

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 require('./database/index'); //Necessário para que os Models verifiquem a conexão com o banco
 
 const app = express();
 
+app.use(cors()); //Limitar acesso
 app.use(express.json());//Trarar requisições com JSON
 app.use(routes);
 
