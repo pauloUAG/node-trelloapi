bf92be6cdcda02fc9703603505fffbd441ea19a9
paulo <paulosousa1011@gmail.com>
2020-01-23 17:37:02 -0300

    Conexao com Banco (Postgres) no Heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,9 @@
 //EXPORTANDO UM OBJETO DE CONFIGURAÇÃO DA BASE DE DADOS
 module.exports = {
      dialect: 'postgres',
+     "dialectOptions": {
+          "ssl": true
+     },
      host: 'ec2-107-21-214-101.compute-1.amazonaws.com',
      username: 'wjtjiqpypyvbwv',
      password: 'f5ef93cb065d6f0a894229f49f30d2073804845b9ddbba73525ca61f6d05a4db',
--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
-var db = require('./models');
 
 require('./database/index'); //Necessário para que os Models verifiquem a conexão com o banco
 
@@ -12,14 +11,3 @@ app.use(express.json());//Trarar requisições com JSON
 app.use(routes);
 
 app.listen(process.env.PORT || 3001);
-
-// development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-db.sequelize.sync().then(function() {
-  http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
-  });
-});
